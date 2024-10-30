c787708e8e68900ce5e6a322a66fac690426ed72
hyoguoo <hyoguoo@gmail.com>
2024-10-31 02:23:11 +0900

    feat: OrderInfo 생성자 빌더 추가

--- order-service/src/main/java/com/hyoguoo/orderservice/domain/OrderInfo.java
+++ order-service/src/main/java/com/hyoguoo/orderservice/domain/OrderInfo.java
@@ -21,4 +21,41 @@ public class OrderInfo {
     private LocalDateTime orderedAt;
     private OrderStatus orderStatus;
     private String cancelReason;
+
+    @SuppressWarnings("unused")
+    @Builder(builderMethodName = "requiredArgsBuilder", buildMethodName = "requiredArgsBuild")
+    protected OrderInfo(Long buyerId, Long giftCardId, Long paymentAmount, String orderId, LocalDateTime orderedAt) {
+        this.buyerId = buyerId;
+        this.giftCardId = giftCardId;
+        this.paymentAmount = paymentAmount;
+        this.orderId = orderId;
+        this.orderedAt = orderedAt;
+        this.orderName = "Order for gift card " + giftCardId;
+
+        this.orderStatus = OrderStatus.PENDING;
+
+        validateNewOrder();
+    }
+
+    private void validateNewOrder() {
+        if (this.buyerId == null || this.buyerId <= 0) {
+            throw new IllegalArgumentException("Buyer ID is required");
+        }
+
+        if (this.giftCardId == null || this.giftCardId <= 0) {
+            throw new IllegalArgumentException("Gift card ID is required");
+        }
+
+        if (this.paymentAmount == null || this.paymentAmount < 0) {
+            throw new IllegalArgumentException("Payment amount must be greater than or equal to 0");
+        }
+
+        if (this.orderId == null || this.orderId.isEmpty()) {
+            throw new IllegalArgumentException("Order ID is required");
+        }
+
+        if (this.orderedAt == null) {
+            throw new IllegalArgumentException("Ordered at is required");
+        }
+    }
 }
