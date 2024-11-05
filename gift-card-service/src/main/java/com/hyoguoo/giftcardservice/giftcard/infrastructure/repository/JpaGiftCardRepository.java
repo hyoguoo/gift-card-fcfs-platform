3d57c6ba9bdefab10f1090083de785fad23eaef5
hyoguoo <hyoguoo@gmail.com>
2024-11-06 03:45:21 +0900

    feat: 재고 증감 Repository 메서드 구현

--- gift-card-service/src/main/java/com/hyoguoo/giftcardservice/giftcard/infrastructure/repository/JpaGiftCardRepository.java
+++ gift-card-service/src/main/java/com/hyoguoo/giftcardservice/giftcard/infrastructure/repository/JpaGiftCardRepository.java
@@ -2,7 +2,16 @@ package com.hyoguoo.giftcardservice.giftcard.infrastructure.repository;
 
 import com.hyoguoo.giftcardservice.giftcard.infrastructure.entity.GiftCardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JpaGiftCardRepository extends JpaRepository<GiftCardEntity, Long> {
 
+    @Modifying
+    @Query("update GiftCardEntity g set g.quantity = g.quantity - :quantity where g.id = :id")
+    void decreaseQuantity(Long id, Integer quantity);
+
+    @Modifying
+    @Query("update GiftCardEntity g set g.quantity = g.quantity + :quantity where g.id = :id")
+    void increaseQuantity(Long id, Integer quantity);
 }
