8c9704df499403ef16ba6eb904c446604fa01971
hyoguoo <hyoguoo@gmail.com>
2024-11-06 09:54:27 +0900

    feat: Gift Card 구매 메시지 Listener 및 메시지 구현

--- gift-card-service/src/main/java/com/hyoguoo/giftcardservice/giftcard/presentation/GiftCardEventConsumer.java
+++ gift-card-service/src/main/java/com/hyoguoo/giftcardservice/giftcard/presentation/GiftCardEventConsumer.java
@@ -1,8 +1,11 @@
 package com.hyoguoo.giftcardservice.giftcard.presentation;
 
+import com.hyoguoo.giftcardservice.giftcard.application.dto.command.GiftCardPurchaseCommand;
 import com.hyoguoo.giftcardservice.giftcard.application.dto.command.GiftCardStockDecreaseCommand;
 import com.hyoguoo.giftcardservice.giftcard.application.dto.command.GiftCardStockIncreaseCommand;
+import com.hyoguoo.giftcardservice.giftcard.presentation.port.GiftCardPurchaseService;
 import com.hyoguoo.giftcardservice.giftcard.presentation.port.GiftCardStockService;
+import com.hyoguoo.kafka.message.GiftCardPurchaseEventMessage;
 import com.hyoguoo.kafka.message.GiftCardStockDecreaseEventMessage;
 import com.hyoguoo.kafka.message.GiftCardStockRollbackEventMessage;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Component;
 public class GiftCardEventConsumer {
 
     private final GiftCardStockService giftCardStockService;
+    private final GiftCardPurchaseService giftCardPurchaseService;
 
     @KafkaListener(topics = "${kafka.topics.gift-card-stock-decrease}", groupId = "${kafka.groups.gift-card-stock-decrease}")
     public void consumeStockDecreaseEvent(GiftCardStockDecreaseEventMessage message) {
@@ -37,5 +41,16 @@ public class GiftCardEventConsumer {
                 .build();
 
         giftCardStockService.increaseQuantity(command);
+    }
+
+    @KafkaListener(topics = "${kafka.topics.gift-card-purchase}", groupId = "${kafka.groups.gift-card-purchase}")
+    public void consumeGiftCardPurchaseEvent(GiftCardPurchaseEventMessage message) {
+        log.info("Consuming purchase: {} by user: {}", message.getGiftCardId(), message.getUserId());
+        GiftCardPurchaseCommand command = GiftCardPurchaseCommand.builder()
+                .giftCardId(message.getGiftCardId())
+                .userId(message.getUserId())
+                .build();
+
+        giftCardPurchaseService.purchaseGiftCard(command);
     }
 }
