a71dafc3fab515823910edac1373d1c8f7829bbc
hyoguoo <hyoguoo@gmail.com>
2024-11-11 13:52:32 +0900

    feat: Payment Checkout 중 에러 발생 시 생성 된 Order Info 실패 처리

--- order-service/src/main/java/com/hyoguoo/orderservice/order/application/OrderCheckoutServiceImpl.java
+++ order-service/src/main/java/com/hyoguoo/orderservice/order/application/OrderCheckoutServiceImpl.java
@@ -4,14 +4,17 @@ import com.hyoguoo.orderservice.order.application.dto.command.CheckoutCommand;
 import com.hyoguoo.orderservice.order.application.dto.command.PaymentCheckoutCommand;
 import com.hyoguoo.orderservice.order.application.dto.result.CheckoutResult;
 import com.hyoguoo.orderservice.order.application.usecase.OrderInfoCreateUseCase;
+import com.hyoguoo.orderservice.order.application.usecase.OrderInfoProcessUseCase;
 import com.hyoguoo.orderservice.order.application.usecase.OrderedGiftCardLoadUseCase;
 import com.hyoguoo.orderservice.order.application.usecase.PaymentCheckoutUseCase;
 import com.hyoguoo.orderservice.order.domain.OrderInfo;
 import com.hyoguoo.orderservice.order.domain.dto.GiftCardInfo;
 import com.hyoguoo.orderservice.order.presentation.port.OrderCheckoutService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderCheckoutServiceImpl implements OrderCheckoutService {
@@ -19,12 +22,19 @@ public class OrderCheckoutServiceImpl implements OrderCheckoutService {
     private final OrderInfoCreateUseCase orderInfoCreateUseCase;
     private final OrderedGiftCardLoadUseCase orderedGiftCardLoadUseCase;
     private final PaymentCheckoutUseCase paymentCheckoutUseCase;
+    private final OrderInfoProcessUseCase orderInfoProcessUseCase;
 
     @Override
     public CheckoutResult checkoutOrder(CheckoutCommand command) {
         GiftCardInfo giftCardInfo = orderedGiftCardLoadUseCase.loadOrderedGiftCard(command.getGiftCardId());
         OrderInfo savedOrderInfo = orderInfoCreateUseCase.createOrderInfo(command, giftCardInfo);
-        checkoutPayment(command, savedOrderInfo, giftCardInfo);
+
+        try {
+            checkoutPayment(command, savedOrderInfo, giftCardInfo);
+        } catch (Exception e) {
+            handleUnknownException(savedOrderInfo);
+            throw e;
+        }
 
         return CheckoutResult.builder()
                 .orderId(savedOrderInfo.getOrderId())
@@ -42,5 +52,10 @@ public class OrderCheckoutServiceImpl implements OrderCheckoutService {
                 .build();
 
         paymentCheckoutUseCase.checkoutPayment(paymentCheckoutCommand);
+    }
+
+    private void handleUnknownException(OrderInfo savedOrderInfo) {
+        log.error("Failed to checkout payment. orderId={}", savedOrderInfo.getOrderId());
+        orderInfoProcessUseCase.failedOrderInfo(savedOrderInfo);
     }
 }
